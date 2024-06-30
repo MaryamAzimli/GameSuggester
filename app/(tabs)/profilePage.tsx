5b20e4ed36079d5abcca2dcfd23102fed56736d4
Boran Deniz Düzgün <>
2024-06-30 17:42:34 +0300

    profilePage updated

--- app/(tabs)/profilePage.tsx
+++ app/(tabs)/profilePage.tsx
@@ -1,13 +1,37 @@
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { StyleSheet, Image, Platform } from "react-native";
 import React from "react";
 import { View, Text } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { ThemedView } from "@/components/ThemedView";
+import { ThemedText } from "@/components/ThemedText";
 
 const profilePage = () => {
   return (
-    <View>
-      <h1>Profile</h1>
-      <Text>This is the profile page added by me!</Text>
-    </View>
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+      headerImage={
+        <FontAwesome5 size={310} name="user-alt" style={styles.headerImage} />
+      }
+    >
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Profile Page</ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
   );
 };
 
 export default profilePage;
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: "#808080",
+    bottom: -90,
+    left: -35,
+    position: "absolute",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+});
