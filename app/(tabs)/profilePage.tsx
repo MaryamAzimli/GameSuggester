ed7dc5a7f2e97ad3d646aae82d4a24d360e23ca1
Boran Deniz Düzgün <>
2024-07-03 22:45:10 +0300

    follow info added

--- app/(tabs)/profilePage.tsx
+++ app/(tabs)/profilePage.tsx
@@ -12,10 +12,20 @@ const ProfilePage = () => {
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
       headerImage={
-        <View style={styles.headerContainer}>
+        <ThemedView style={styles.headerContainer}>
           <Image source={octopusImage} style={styles.profileImage} />
           <ThemedText type="title">Octo the Gamer</ThemedText>
-        </View>
+          <View style={styles.statsContainer}>
+            <View style={styles.statItem}>
+              <ThemedText style={styles.statNumber}>87.8K</ThemedText>
+              <ThemedText style={styles.statLabel}>Followers</ThemedText>
+            </View>
+            <View style={styles.statItem}>
+              <ThemedText style={styles.statNumber}>526</ThemedText>
+              <ThemedText style={styles.statLabel}>Following</ThemedText>
+            </View>
+          </View>
+        </ThemedView>
       }
     >
       <ThemedView style={styles.titleContainer}></ThemedView>
@@ -33,11 +43,28 @@ const styles = StyleSheet.create({
     backgroundColor: "#353636",
   },
   profileImage: {
-    width: 150,
-    height: 150,
+    width: 100,
+    height: 100,
     borderRadius: 75,
     borderColor: "#808080",
     borderWidth: 2,
+  },
+  statsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+  },
+  statItem: {
+    alignItems: "center",
+  },
+  statNumber: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#FFA500",
+  },
+  statLabel: {
+    fontSize: 14,
+    color: "#FFFFFF",
   },
   titleContainer: {
     flex: 1,
