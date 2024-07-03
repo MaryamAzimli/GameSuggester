b8cedd0353bc720fc4ba67fe4eadc1bce49b6e68
Boran Deniz Düzgün <>
2024-07-03 23:06:54 +0300

    navbar added to profile page

--- app/(tabs)/profilePage.tsx
+++ app/(tabs)/profilePage.tsx
@@ -1,13 +1,14 @@
+import React, { useState } from "react";
+import { StyleSheet, Image, TouchableOpacity, View } from "react-native";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { StyleSheet, Image, Platform } from "react-native";
-import React from "react";
-import { View, Text } from "react-native";
-import { FontAwesome5 } from "@expo/vector-icons";
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
 import octopusImage from "@/assets/images/octopus.png";
+import { FontAwesome } from "@expo/vector-icons";
 
 const ProfilePage = () => {
+  const [selectedTab, setSelectedTab] = useState("Games");
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
@@ -28,7 +29,44 @@ const ProfilePage = () => {
         </ThemedView>
       }
     >
-      <ThemedView style={styles.titleContainer}></ThemedView>
+      <ThemedView style={styles.tabContainer}>
+        <TouchableOpacity
+          style={[styles.tab, selectedTab === "Games" && styles.selectedTab]}
+          onPress={() => setSelectedTab("Games")}
+        >
+          <ThemedText
+            style={[
+              styles.tabText,
+              selectedTab === "Games" && styles.selectedTabText,
+            ]}
+          >
+            Games
+          </ThemedText>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.tab, selectedTab === "Lists" && styles.selectedTab]}
+          onPress={() => setSelectedTab("Lists")}
+        >
+          <ThemedText
+            style={[
+              styles.tabText,
+              selectedTab === "Lists" && styles.selectedTabText,
+            ]}
+          >
+            Lists
+          </ThemedText>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.tab, selectedTab === "Plus" && styles.selectedTab]}
+          onPress={() => setSelectedTab("Plus")}
+        >
+          <FontAwesome
+            name="plus"
+            size={24}
+            color={selectedTab === "Plus" ? "#FFA500" : "white"}
+          />
+        </TouchableOpacity>
+      </ThemedView>
     </ParallaxScrollView>
   );
 };
@@ -66,13 +104,27 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: "#FFFFFF",
   },
-  titleContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100%",
+  tabContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingVertical: 10,
   },
-  titleText: {
-    textAlign: "center",
+  tab: {
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+  },
+  selectedTab: {
+    borderColor: "#FFA500",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+  },
+  tabText: {
+    fontSize: 16,
+    color: "#FFFFFF",
+  },
+  selectedTabText: {
+    color: "#FFA500",
   },
 });
