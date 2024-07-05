05c36104de13ed62f83f8f9e078cc4d843f73d95
Boran Deniz Düzgün <>
2024-07-05 09:17:21 +0300

    profile page notification updated

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -8,8 +8,8 @@ const tintColorDark = "#fff";
 
 export const Colors = {
   light: {
-    text: "#11181C",
-    background: "#fff",
+    text: "#000000",
+    background: "#EEEDEB",
     tint: tintColorLight,
     icon: "#687076",
     tabIconDefault: "#687076",
