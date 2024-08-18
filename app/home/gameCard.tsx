aa7d8d61d215e09082c88d87a9284375fe4ad145
BrnDnzDzgn <bdduzgun@outlook.com>
2024-08-18 19:17:46 +0300

    error fixed

--- app/home/gameCard.tsx
+++ app/home/gameCard.tsx
@@ -82,7 +82,14 @@ const GameCard = () => {
     
   };
   
-  
+  const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
   
 
   const handleTagClick = (tag) => {
